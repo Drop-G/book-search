f1bbab054d10f02bd75a9b1902b91e500d2cb547
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-12-16 11:14:05 -0500

    updated

--- models/book.js
+++ models/book.js
@@ -1,52 +1,16 @@
-const db = require("../models");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// Defining methods for the booksController
-module.exports = {
-  findAll: function(req, res) {
-    db.Book
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => {
-        console.error(err)
-        res.status(422).json(err)
-      });
-  },
-  findById: function(req, res) {
-    db.Book
-      .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => {
-        console.error(err)
-        res.status(422).json(err)
-      });
-  },
-  create: function(req, res) {
-    db.Book
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => {
-        console.error(err)
-        res.status(422).json(err)
-      });
-  },
-  update: function(req, res) {
-    db.Book
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => {
-        console.error(err)
-        res.status(422).json(err)
-      });
-  },
-  remove: function(req, res) {
-    db.Book
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => {
-        console.error(err)
-        res.status(422).json(err)
-      });
-  }
-};
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  subtitle: { type: String },
+  authors: { type: [String], required: true },
+  link: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  googleId: { type: String, required: true, unique: true }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
-app.use(routes)
+// Add routes, both API and view
+app.use(routes);
 
-mongoose.connect(process.env.MONGODBURL || "mongodb://localhost/Book", { useNewUrlParser: true, useUnifiedTopology: true });
-// to check status of your mongodb connection
-mongoose.connection.on('connected', function () { console.log("Mongo DB connected") });
-mongoose.connection.on('error', function (err) { console.error(err) });
-mongoose.connection.on('disconnected', function () { console.log("Mongo DB disconnected") });
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
-app.listen(process.env.PORT || 3001, function () {
-    console.log('Express Server Listening on 3001');
-})
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
